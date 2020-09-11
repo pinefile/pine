c449b01f44142721acdb704dcbd848887825be60
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-09-11 09:19:58 +0200

    use process.cwd instead of file path for pkg func

--- src/plugins/core.ts
+++ src/plugins/core.ts
@@ -3,24 +3,16 @@ import path from 'path';
 import { PineType } from '../';
 
 export function pkg(this: PineType): any {
-  return require(path.dirname(this.file()) + '/package.json');
+  return require(process.cwd() + '/package.json');
 }
 
 export function readJSON(this: PineType, file: string): string {
-  if (file.startsWith('.')) {
-    file = path.dirname(this.file()) + file.slice(1);
-  }
-
   const content = fs.readFileSync(file).toString();
 
   return JSON.parse(content);
 }
 
 export function writeJSON(this: PineType, file: string, content: any): boolean {
-  if (file.startsWith('.')) {
-    file = path.dirname(this.file()) + file.slice(1);
-  }
-
   const data = JSON.stringify(content, null, 2);
 
   fs.writeFileSync(file, data);
--- test/fixtures/pinefile.plugins.core.js
+++ test/fixtures/pinefile.plugins.core.js
@@ -3,4 +3,4 @@ exports.pkg = () => console.log(`pkg: ${pkg().version}`);
 exports.readJSON = () =>
   console.log(`readJSON: ${readJSON('./package.json').version}`);
 
-exports.writeJSON = () => writeJSON('./file.json', { version: '1.0.0' });
+exports.writeJSON = () => writeJSON('./test.json', { version: '1.0.0' });
