589a23d4fb3e7b3db9fc77fdb4139996ccb0acf4
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-09-11 14:58:03 +0200

    Use process.cwd as file path

--- src/plugins/core.ts
+++ src/plugins/core.ts
@@ -3,16 +3,24 @@ import path from 'path';
 import { PineType } from '../';
 
 export function pkg(this: PineType): any {
-  return require(process.cwd() + '/package.json');
+  return require(path.join(process.cwd(), 'package.json'));
 }
 
 export function readJSON(this: PineType, file: string): string {
+  if (!file.startsWith('/')) {
+    file = path.join(process.cwd(), file);
+  }
+
   const content = fs.readFileSync(file).toString();
 
   return JSON.parse(content);
 }
 
 export function writeJSON(this: PineType, file: string, content: any): boolean {
+  if (!file.startsWith('/')) {
+    file = path.join(process.cwd(), file);
+  }
+
   const data = JSON.stringify(content, null, 2);
 
   fs.writeFileSync(file, data);
