3fce2244f878f62728c925eeb45a9207e30b6968
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-09-19 18:48:12 +0200

    Fix failing test

--- test/plugins/file.test.ts
+++ test/plugins/file.test.ts
@@ -3,6 +3,6 @@ import { pkg } from '../../src/plugins/file';
 describe('file', () => {
   it('can read closest package.json', async () => {
     const p = pkg();
-    expect(p.bin).toBe('bin/pine');
+    expect(p.bin).toEqual({ pine: 'bin/pine' });
   });
 });
