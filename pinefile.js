7447440d7061d674b07bb5dbb5d25488f3acbe3b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-10-27 20:20:26 +0100

    Include npm scripts in our pinefile for dev

--- pinefile.js
+++ pinefile.js
@@ -1,11 +1,19 @@
 const isDev = process.env.PINE_ENV === 'development';
-const { run, shell, log } = require(`./packages/pine${isDev ? '/src' : ''}`);
+const { pkg, run, shell, log } = require(`./packages/pine${isDev ? '/src' : ''}`);
 
 const getLatestCommit = async () => await shell(`git rev-parse --short HEAD`);
 
 const npm = (c) => run(`npm run ${c}`);
 
+let { scripts } = pkg();
+
+Object.keys(scripts).forEach(key => {
+  const cmd = scripts[key];
+  scripts[key] = async () => await run(cmd);
+});
+
 module.exports = {
+  ...scripts,
   build: async () => {
     await npm('build');
   },
@@ -16,5 +24,4 @@ module.exports = {
     const commit = await getLatestCommit();
     log.log(`hello ${commit}`);
   },
-  lint: async () => await run(`eslint packages/**/src --ext .ts`),
 };
