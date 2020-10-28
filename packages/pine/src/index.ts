a6b8b572b5c9ef2f9afdec121ff20f0321978599
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2020-10-28 20:31:50 +0100

    Format code

--- packages/pine/src/index.ts
+++ packages/pine/src/index.ts
@@ -142,8 +142,16 @@ export const runTask = (argv: Array<any>): void => {
   const args = yargs
     .options({
       help: { type: 'boolean', default: false, desc: 'Show help' },
-      file: { type: 'string', default: '', desc: 'Path to Pipefile or pipefile.js' },
-      silent: { type: 'boolean', default: false, desc: 'Runs the task in silent mode' },
+      file: {
+        type: 'string',
+        default: '',
+        desc: 'Path to Pipefile or pipefile.js',
+      },
+      silent: {
+        type: 'boolean',
+        default: false,
+        desc: 'Runs the task in silent mode',
+      },
     })
     .parse(argv);
   const name = args._.shift();
