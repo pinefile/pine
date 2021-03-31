1dc0c1e2007cc3d0e08fa20a28b67f331e39ec2f
Fredrik Forsmo <frozzare@users.noreply.github.com>
2021-03-31 14:12:42 +0200

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  presets: [
-    '@babel/preset-typescript',
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-  ],
-};
