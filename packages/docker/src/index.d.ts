71989407ce30952a906fb8cd342309fa06d9cc8b
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-04-04 14:09:45 +0200

    Remove old docker file

--- packages/docker/src/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare type RunDockerOptionsType = {
-    args?: string;
-    image: string;
-    versions: Array<string>;
-    cmd: string;
-};
-export declare const run: (cmd: string, opts: RunDockerOptionsType) => Promise<void>;
-export {};
