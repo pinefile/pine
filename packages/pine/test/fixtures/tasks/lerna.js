52b2b86b5837fa515c70e5fc51710c0554865dc6
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-05-03 11:23:21 +0200

    Delay lerna:postdefault

--- packages/pine/test/fixtures/tasks/lerna.js
+++ packages/pine/test/fixtures/tasks/lerna.js
@@ -1,7 +1,12 @@
+const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+
 module.exports = {
   predefault: () => console.log('lerna:predefault'),
   default: () => console.log('lerna:default'),
-  postdefault: () => console.log('lerna:postdefault'),
+  postdefault: async () => {
+    await delay(500);
+    console.log('lerna:postdefault');
+  },
   prebuild: () => console.log('lerna:prebuild'),
   build: () => console.log('lerna:build'),
   postbuild: () => console.log('lerna:postbuild'),
